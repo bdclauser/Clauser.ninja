337faf89676096b70ab74752909e597692088d8e
Brian Clauser <pzljug@hotmail.com>
2022-03-08 21:45:43 -0500

    updated js

--- js/mail.js
+++ js/mail.js
@@ -1,14 +1,8 @@
 (function submitted() {
     'use strict';
-    window.addEventListener('load', function() {
-        var form = document.getElementsByTagName('form')
-        formaddEventListener('submit', function(event){
-            event.preventDefault();
-            event.stopPropagation();
-            if (form.checkValidity() === true) {
-
-            }
-            form.classList.add('was-validated');
-        }, false);
-    }, false);
+    windows.onbeforeunload = () => {
+        for(const form of document.getElementsByTagName('form')) {
+            form.reset();
+        }
+    }
 })();
