e12709356c7d1901231d31041d69b0208c5b9e9c
Brian Clauser <pzljug@hotmail.com>
2022-03-08 21:31:28 -0500

    added js to mail

---
+++ js/mail.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        var form = document.getElementsByTagName('form')
+        formaddEventListener('submit', function(event){
+            event.preventDefault();
+            event.stopPropagation();
+            if (form.checkValidity() === true) {
+
+            }
+            form.classList.add('was-validated');
+        }, false);
+    }, false);
+})();
