982063e66a18b283483a5adaec2ba142b5140deb
bdclauser <pzljug@hotmail.com>
2023-02-28 00:35:09 -0500

    continuous improvement

--- js/main.js
+++ js/main.js
@@ -11,6 +11,27 @@ $(document).ready(function () {
   });
 });
 
+/* document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector(".header");
+  header.style.height = `${window.innerHeight}px`;
+
+  const navbarLinks = document.querySelectorAll(".navbar a");
+
+  navbarLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const sectionId = e.target.dataset.value;
+      const sectionOffsetTop = document.getElementById(sectionId).offsetTop;
+
+      window.scrollTo({
+        top: sectionOffsetTop,
+        behavior: "smooth",
+      });
+    });
+  });
+});
+ */
+
 document.addEventListener("DOMContentLoaded", function () {
   let date = new Date();
   let year = date.getFullYear();
