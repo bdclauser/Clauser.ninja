282c627f2fa7c1f98119fbf26bc36cc83e2b1263
bdclauser <pzljug@hotmail.com>
2023-06-27 22:46:05 -0400

    added vickie project

---
+++ vickie/myscripts.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("exampleDiv");
+  if (window.getComputedStyle(x).display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
