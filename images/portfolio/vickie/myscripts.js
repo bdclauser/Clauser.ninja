bb4e0aef517a3bb6f3552b10ac78f4cea84f80b1
bdclauser <pzljug@hotmail.com>
2023-06-28 17:01:59 -0400

    css updates and typo fixes

--- images/portfolio/vickie/myscripts.js
+++ images/portfolio/vickie/myscripts.js
@@ -1,8 +1,10 @@
-function myFunction() {
+
+// Not used at this time
+/* function myFunction() {
   var x = document.getElementById("exampleDiv");
   if (window.getComputedStyle(x).display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
-}
+} */
